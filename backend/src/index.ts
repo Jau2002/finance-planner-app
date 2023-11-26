39e72b5cc4f861e052d7581114e2b260eda1aee1
Jau2002 <idamendjaume@gmail.com>
2023-11-26 18:24:31 -0500

    fix: deploy loop

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,10 @@
 import * as dotenv from 'dotenv'
 import app from './middlewares/app'
-import prisma from './middlewares/client'
 
 dotenv.config({ path: '.env' })
 
-const PORT: number = parseInt(process.env.PORT || '3001', 10) ?? 3001
+const PORT: number = parseInt(process.env.PORT || '3001') ?? 3001
 
-const main = async (): Promise<void> => {
-	try {
-		await prisma.$connect()
-		console.log('Connected to the database')
-
-		app.listen(PORT, 'localhost', function (this: any): void {
-			console.log(
-				`Server is running at http://localhost:${this.address().port}`
-			)
-		})
-	} catch (error) {
-		console.error('Error connecting to the database:', error)
-		process.exit(1)
-	}
-}
-main()
+app.listen(PORT, () => {
+	console.log(`Server is running at http://localhost:${PORT}`)
+})
